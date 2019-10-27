91130ec10135ef6f81c8c149a9b9232617c62cc0
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-10-27 13:43:55 +0100

    Initialiaze tests

---
+++ tests/CommandDispatcherTest.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\JeckelLab\ContainerDispatcher;
+
+use JeckelLab\ContainerDispatcher\CommandDispatcher;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CommandDispatcherTest
+ */
+final class CommandDispatcherTest extends TestCase
+{
+    /**
+     * @test dispatch
+     */
+    public function testDispatch()
+    {
+
+    }
+}
