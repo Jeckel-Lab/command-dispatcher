0821a99b95b3c5798da3044927b14662cbf3a9a4
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-11-14 17:04:14 +0100

    Add InvalidCommandException

---
+++ src/Exception/CommandDispatcherException.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * @author Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at : 14/11/2019
+ */
+
+namespace JeckelLab\CommandDispatcher\Exception;
+
+use RuntimeException;
+
+/**
+ * Class CommandDispatcherException
+ * @package JeckelLab\CommandDispatcher\Exception
+ */
+abstract class CommandDispatcherException extends RuntimeException
+{
+
+}
